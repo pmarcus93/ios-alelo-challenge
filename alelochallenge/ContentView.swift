98f43163194b211845aea731ba915b785fe89818
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-10 10:34:54 -0300

    Initial Commit

---
+++ alelochallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 10/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ alelochallenge/alelochallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  alelochallengeApp.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct alelochallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
