e0f0d008c174ec6cc25d519d16afa39e0256015a
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-13 18:06:22 -0300

    main: divide responsabilidades em views menores em ShoppingCartView

--- alelochallenge/Screens/ShoppingCartView.swift
+++ alelochallenge/Screens/ShoppingCartView.swift
@@ -12,48 +12,68 @@ struct ShoppingCartView: View {
     @Binding var products: [ProductApi]
 
     var body: some View {
-        VStack() {
+        VStack {
             List {
-                if (shoppingCart.isEmpty) {
+                if shoppingCart.isEmpty {
                     Text("Nenhum produto adicionado ao carrinho!")
                 } else {
                     ForEach(shoppingCart, id: \.sku) { shoppingCardProduct in
-                            VStack {
-                                if let product = products.first(where: { $0.codeColor == shoppingCardProduct.productId }) {
-                                    ProductCardView(product: product)
-                                }
-                                HStack() {
-                                    Text("Tam.: \(shoppingCardProduct.size)")
-                                        .font(.caption)
-                                    Text("Quantidade: \(shoppingCardProduct.quantity)")
-                                        .font(.caption)
-                                    Spacer()
-                                }
-                            }
+                        ShoppingCartItemView(product: findProduct(by: shoppingCardProduct.productId), shoppingCardProduct: shoppingCardProduct)
                     }.onDelete { indexSet in
                         shoppingCart.remove(atOffsets: indexSet)
                     }
-                    HStack {
-                        Spacer()
-                        Text("Valor total:")
-                        Text("R$\(shoppingCartTotal())")
-                            .foregroundColor(Color.green)
-                            .fontWeight(.bold)
-                    }
+                    TotalPriceView(shoppingCart: shoppingCart, products: products)
                 }
             }
         }
     }
-    
-    func shoppingCartTotal() -> String {
+
+    private func findProduct(by productId: String) -> ProductApi? {
+        return products.first { $0.codeColor == productId }
+    }
+}
+
+struct ShoppingCartItemView: View {
+    var product: ProductApi?
+    var shoppingCardProduct: productCart
+
+    var body: some View {
+        VStack {
+            if let product = product {
+                ProductCardView(product: product)
+            }
+            HStack {
+                Text("Tam.: \(shoppingCardProduct.size)")
+                    .font(.caption)
+                Text("Quantidade: \(shoppingCardProduct.quantity)")
+                    .font(.caption)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct TotalPriceView: View {
+    var shoppingCart: [productCart]
+    var products: [ProductApi]
+
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("Valor total:")
+            Text("R$\(shoppingCartTotal)")
+                .foregroundColor(Color.green)
+                .fontWeight(.bold)
+        }
+    }
+
+    private var shoppingCartTotal: String {
         var totalPrice: Double = 0
 
         for item in shoppingCart {
             if let product = products.first (where: { $0.codeColor == item.productId }) {
                 if let productPrice = removeCurrencySymbolAndParse(product.actualPrice) {
                     totalPrice += productPrice
-                } else {
-                    print("Erro ao fazer o parse do valor monetário: \(product.actualPrice)")
                 }
             }
         }
