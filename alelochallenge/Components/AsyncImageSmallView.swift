ec2e214c911829660052d74cb6c47e5180774719
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-14 19:31:41 -0300

    modularizando-views: cria AsyncImageSmallView e NoImageSmallView

---
+++ alelochallenge/Components/AsyncImageSmallView.swift
@@ -0,0 +1,32 @@
+//
+//  AsyncImageSmallView.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 14/09/23.
+//
+
+import SwiftUI
+
+struct AsyncImageSmallView: View {
+    var imageUrl: String
+    
+    var body: some View {
+        AsyncImage(url: URL(string: imageUrl)) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .resizable()
+                    .scaledToFit()
+            case .failure(_):
+                Color.red
+            default:
+                Color.white
+            }
+        }
+
+    }
+}
+
+#Preview {
+    AsyncImageSmallView()
+}
--- alelochallenge/Components/ProductCardView.swift
+++ alelochallenge/Components/ProductCardView.swift
@@ -41,27 +41,10 @@ struct ProductCardView: View {
             }
             
             ZStack {
-                
-                if(product.image.isEmpty) {
-                    Image(systemName: "camera.on.rectangle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 25, height: 25)
-                        .foregroundColor(.gray)
-                        .padding()
+                if (product.image.isEmpty) {
+                    NoImageSmallView()
                 } else {
-                    AsyncImage(url: URL(string: product.image)) { phase in
-                        switch phase {
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        case .failure(_):
-                            Color.red
-                        default:
-                            Color.white
-                        }
-                    }
+                    AsyncImageSmallView(imageUrl: product.image)
                 }
             }
             .frame(width: 75, height: 75)
