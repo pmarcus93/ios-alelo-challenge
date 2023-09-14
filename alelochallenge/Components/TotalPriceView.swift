12ad553a4c0af78431cd3aefed89af76e7e3bed0
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-14 19:24:50 -0300

    modularizando-views: cria TotalPriceView

---
+++ alelochallenge/Components/TotalPriceView.swift
@@ -0,0 +1,18 @@
+//
+//  TotalPriceView.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 14/09/23.
+//
+
+import SwiftUI
+
+struct TotalPriceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TotalPriceView()
+}
