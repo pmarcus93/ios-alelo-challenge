453e6a5eb44c964f856a0f379680b30746eabc1f
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-14 19:17:56 -0300

    modularizando-views: cria ShoppingCartItemView

---
+++ alelochallenge/Components/ShoppingCartItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingCartItemView.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 14/09/23.
+//
+
+import SwiftUI
+
+struct ShoppingCartItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingCartItemView()
+}
