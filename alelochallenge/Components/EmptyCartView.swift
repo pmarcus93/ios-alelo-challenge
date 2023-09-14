3907161edb1b1031bc09c6a1341f184df7d4b0b8
Marcus Pereira <pereiramarcus93@gmail.com>
2023-09-14 19:11:29 -0300

    modularizando-views: cria EmptyCartView

---
+++ alelochallenge/Components/EmptyCartView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCartView.swift
+//  alelochallenge
+//
+//  Created by Marcus Vinicius Pereira on 14/09/23.
+//
+
+import SwiftUI
+
+struct EmptyCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyCartView()
+}
